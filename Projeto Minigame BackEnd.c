fb5023e744348b84f75845ab414ca93483777cdf
Gabriel Marques <gabrielmarx04@gmail.com>
2026-05-11 16:11:45 -0300

    Adição de personagens + menu básico
    Emojis vão ser utilizados ao invés de caracteres normais e a matriz teve um aumento de tamanho. Foi adicionado um menu.

--- Projeto Minigame BackEnd.c
+++ Projeto Minigame BackEnd.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <string.h>
 
 int main() {
-   char *mapa[5][5] = {{NULL}}; // Essencial setar o ponteiro como null antes de utilizar
-   int i, j;
+   char *mapa[10][10] = {{NULL}}; // Essencial setar o ponteiro como null antes de utilizar
+   int i, j, menu;
    
-/* F = Caçador
-   L = Lobo
-   O = Ovelha */
    
-   mapa[0][0] = "F";
-   mapa[1][1] = "L";
-   mapa[2][2] = "O";
+   mapa[0][0] = "👮";
+   mapa[1][1] = "█";
+   mapa[2][2] = "🏃";
+   mapa[3][3] = "💰";
 
-   for(i = 0; i < 5; i++){
-        for(j = 0; j < 5; j++){
+printf("——— Policia e Ladrão ———\nDigite 1 para jogar ou 0 para sair\n");
+scanf("%d", &menu);
+
+switch(menu){
+    
+    case 0:
+        printf("Saindo...\n");
+        break;
+    
+    case 1:
+    printf("\n");
+   for(i = 0; i < 10; i++){
+        for(j = 0; j < 10; j++){
             if (mapa[i][j] != NULL){ // Se o conteúdo da matriz não estiver vazio, printe o conteúdo dela 
             printf("%s\t", mapa[i][j]);
             }
             else{ // Caso esteja vazio, printe "."
-                printf(".");
-                printf("\t");
+                printf(".\t");
             }
         }
     printf("\n");
     }
-
-   
+    break;
+}// Fim do case aqui
     return 0;
 }
