714a2d528df777cd010a4a33f6084e4cb1d13b03
Gabriel Marques <gabrielmarx04@gmail.com>
2026-05-14 15:34:52 -0300

    Melhoria na identação

--- Projeto Minigame BackEnd.c
+++ Projeto Minigame BackEnd.c
@@ -8,156 +8,155 @@
 
 int  altura, largura; // Variáveis de altura e largura do mapa, declaradas de forma global para que sejam lidas pelas funções
 
-typedef struct Player{ // Struct com nome Player (para ser lido corretamente pela função RenderizarMapa)
-    int vida;
-    int pontuacao;
-    int posicaox;
-    int posicaoy;
-    int posicaoxtemp;
-    int posicaoytemp;
+typedef struct Player { // Struct com nome Player (para ser lido corretamente pela função RenderizarMapa)
+	int vida;
+	int pontuacao;
+	int posicaox;
+	int posicaoy;
+	int posicaoxtemp;
+	int posicaoytemp;
 } Player; // Alias da struct, para ser lido corretamente pelas demais funções
 
-void renderizarMapa(char *m, struct Player *p){ // Struct sendo passado como parâmetro para ser lido pelo laço for
-    for(int i = 0; i < altura; i++){
-        for(int j = 0; j < largura; j++){
-            if(i == p->posicaox && j == p->posicaoy){ // Se i e j forem iguais a posição do jogador, imprima 🏃
-            printf("🏃\t");
-        }
-            else if(m[i * largura + j] == MOEDA){ // Caso encontre um "$" no código, substitui pelo emoji
-                printf("💰\t");
-            }
-            else if(m[i * largura + j] == POLICIAL){  // Caso encontre um "2" no código, substitui pelo emoji
-                printf("👮\t");
-            }
-            else if(m[i * largura + j] == PAREDE){  // Caso encontre um "1" no código, substitui pelo emoji
-                printf("█\t");
-            }
-            else if (m[i * largura + j] == VAZIO){
-            printf(".\t"); // Se o conteúdo da matriz não for vazio E NÃO FOR IGUAL A POSIÇÃO DO JOGADOR, imprima seu conteúdo
-            }
-            
-            else{ // Caso esteja vazio, printe "."
-                printf(".\t");
-            }
-        }
-    printf("\n"); // Quebra de linha para organização
-    }
+void renderizarMapa(char *m, struct Player *p) { // Struct sendo passado como parâmetro para ser lido pelo laço for
+	for(int i = 0; i < altura; i++) {
+		for(int j = 0; j < largura; j++) {
+			if(i == p->posicaox && j == p->posicaoy) { // Se i e j forem iguais a posição do jogador, imprima 🏃
+				printf("🏃\t");
+			}
+			else if(m[i * largura + j] == MOEDA) { // Caso encontre um "$" no código, substitui pelo emoji
+				printf("💰\t");
+			}
+			else if(m[i * largura + j] == POLICIAL) { // Caso encontre um "2" no código, substitui pelo emoji
+				printf("👮\t");
+			}
+			else if(m[i * largura + j] == PAREDE) { // Caso encontre um "1" no código, substitui pelo emoji
+				printf("█\t");
+			}
+			else if (m[i * largura + j] == VAZIO) {
+				printf(".\t"); // Se o conteúdo da matriz não for vazio E NÃO FOR IGUAL A POSIÇÃO DO JOGADOR, imprima seu conteúdo
+			}
+
+			else { // Caso esteja vazio, printe "."
+				printf(".\t");
+			}
+		}
+		printf("\n"); // Quebra de linha para organização
+	}
 }
 
-void MoverParaCima(Player *c, char *m){
-  if(m[(c->posicaox - 1) * largura + c->posicaoy] != PAREDE){
-    c->posicaox--;
-  }
-    else{
-    printf("Bateu na parede, movimento invalido!\n");
-    }
+void MoverParaCima(Player *c, char *m) {
+	if(m[(c->posicaox - 1) * largura + c->posicaoy] != PAREDE) {
+		c->posicaox--;
+	}
+	else {
+		printf("Bateu na parede, movimento invalido!\n");
+	}
 }
 
-void MoverParaBaixo(Player *c,char *m){
-    if(m[(c->posicaox + 1) * largura + c->posicaoy] != PAREDE){
-    c->posicaox++;
-    }
-    else{
-        printf("Bateu na parede, movimento invalido!\n");
-    }
+void MoverParaBaixo(Player *c,char *m) {
+	if(m[(c->posicaox + 1) * largura + c->posicaoy] != PAREDE) {
+		c->posicaox++;
+	}
+	else {
+		printf("Bateu na parede, movimento invalido!\n");
+	}
 }
 
-void MoverParaEsquerda(Player *c, char *m){
-     if(m[(c->posicaox) * largura + c->posicaoy - 1] != PAREDE){
-        c->posicaoy--;
-     }
-    else{    
-        printf("Bateu na parede, movimento invalido!\n");
-    }
+void MoverParaEsquerda(Player *c, char *m) {
+	if(m[(c->posicaox) * largura + c->posicaoy - 1] != PAREDE) {
+		c->posicaoy--;
+	}
+	else {
+		printf("Bateu na parede, movimento invalido!\n");
+	}
 }
 
-void MoverParaDireita(Player *c, char *m){
-    if(m[(c->posicaox) * largura + c->posicaoy + 1] != PAREDE){
-    c->posicaoy++;
-    }
-    else{    
-        printf("Bateu na parede, movimento invalido!\n");
-    }
+void MoverParaDireita(Player *c, char *m) {
+	if(m[(c->posicaox) * largura + c->posicaoy + 1] != PAREDE) {
+		c->posicaoy++;
+	}
+	else {
+		printf("Bateu na parede, movimento invalido!\n");
+	}
 }
 
 int main() {
-   char *mapa; 
-   int i, j, menu;
-   int escolha = 1;
-   char direcao;
-   FILE *nivel;
-   
-   //JOGADOR
-   Player p;
-   p.posicaox = 4; // Posição vertical do jogador
-   p.posicaoy = 3; // Posição horizontal do jogador
-  
-   
-   nivel = fopen("nivel1.txt", "r"); // Abre o arquivo com o mapa
-   
-   if(nivel == NULL){
-       printf("Erro ao abrir arquivo\n");
-       return 1; // Retorna um erro caso não seja possível abrir o arquivo
-   }
-   
-   fscanf(nivel, " %d %d", &largura, &altura); // Lê os primeiros dois números do arquivo e os considera como tamanho da altura e da largura, alocando cada valor nas variáveis  altura e largura, respectivamente
-   
-     
-    mapa = (char*)malloc(largura * altura * sizeof(char)); // Aloca memória o suficiente no ponteiro mapa, de acordo com os valores de altura e largura
-   
-   
-   for(i = 0; i < altura; i++){ 
-       for(j = 0; j < largura; j++){
-           if(fscanf(nivel, " %c", &mapa[(i * largura + j)]) != 1){ // Caso a leitura do arquivo não retorne um caractere válido (True) imprima um texto de erro
-               printf("Erro na leitura do mapa!");
-               break;
-           }
-       }
-   }
-   
-   
-   
-   /*👮█💰🏃*/
-   
+	char *mapa;
+	int i, j, menu;
+	int escolha = 1;
+	char direcao;
+	FILE *nivel;
 
-printf("——— Policia e Ladrão ———\nDigite 1 para jogar ou 0 para sair\n");
-scanf("%d", &menu);
+	//JOGADOR
+	Player p;
+	p.posicaox = 4; // Posição vertical do jogador
+	p.posicaoy = 3; // Posição horizontal do jogador
 
-switch(menu){
-    
-    case 0:
-        printf("Saindo...\n");
-        break;
-    
-    case 1:
-        while (escolha == 1){
-            printf("\n");
-            renderizarMapa(mapa, &p);
-    
-            printf("Para onde ir? Digite 'w a s d' e digite ENTER\n");
-            scanf(" %c", &direcao);
-    
-            if(direcao == 'w'){
-                MoverParaCima(&p, mapa);
-                }
-               
-            if(direcao == 's'){
-                MoverParaBaixo(&p, mapa);
-            }
-            
-            if(direcao == 'a'){
-               MoverParaEsquerda(&p, mapa);
-            }
-            
-            if(direcao == 'd'){
-               MoverParaDireita(&p, mapa);
-            }
-            
-    
-            printf("\nNova posicao X do player = %d\n", p.posicaox);
-            printf("Nova posicao Y do player = %d\n", p.posicaoy);
-        }
-    free(mapa);
-}// Fim do case aqui
-    return 0;
+
+	nivel = fopen("nivel1.txt", "r"); // Abre o arquivo com o mapa
+
+	if(nivel == NULL) {
+		printf("Erro ao abrir arquivo\n");
+		return 1; // Retorna um erro caso não seja possível abrir o arquivo
+	}
+
+	fscanf(nivel, " %d %d", &largura, &altura); // Lê os primeiros dois números do arquivo e os considera como tamanho da altura e da largura, alocando cada valor nas variáveis  altura e largura, respectivamente
+
+
+	mapa = (char*)malloc(largura * altura * sizeof(char)); // Aloca memória o suficiente no ponteiro mapa, de acordo com os valores de altura e largura
+
+
+	for(i = 0; i < altura; i++) {
+		for(j = 0; j < largura; j++) {
+			if(fscanf(nivel, " %c", &mapa[(i * largura + j)]) != 1) { // Caso a leitura do arquivo não retorne um caractere válido (True) imprima um texto de erro
+				printf("Erro na leitura do mapa!");
+				break;
+			}
+		}
+	}
+
+
+	/*👮█💰🏃*/
+
+
+	printf("——— Policia e Ladrão ———\nDigite 1 para jogar ou 0 para sair\n");
+	scanf("%d", &menu);
+
+	switch(menu) {
+
+	case 0:
+		printf("Saindo...\n");
+		break;
+
+	case 1:
+		while (escolha == 1) {
+			printf("\n");
+			renderizarMapa(mapa, &p);
+
+			printf("Para onde ir? Digite 'w a s d' e digite ENTER\n");
+			scanf(" %c", &direcao);
+
+			if(direcao == 'w') {
+				MoverParaCima(&p, mapa);
+			}
+
+			if(direcao == 's') {
+				MoverParaBaixo(&p, mapa);
+			}
+
+			if(direcao == 'a') {
+				MoverParaEsquerda(&p, mapa);
+			}
+
+			if(direcao == 'd') {
+				MoverParaDireita(&p, mapa);
+			}
+
+
+			printf("\nNova posicao X do player = %d\n", p.posicaox);
+			printf("Nova posicao Y do player = %d\n", p.posicaoy);
+		}
+		free(mapa);
+	}// Fim do case aqui
+	return 0;
 }
